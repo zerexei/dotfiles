0c0c6361374c30856822935562bd1df46e748c0a
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-04-22 23:09:39 +0800

    add vimrc install script

---
+++ vimrc-setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# create a backup dir
+mkdir -p "$HOME/.backup"
+
+# move old vimrc to backup dir
+mv "$HOME/.vimrc" "$HOME/.backup/.vimrc" 2>/dev/null
+
+# create a symlink for vimrc
+ln -sf "$PWD/.vimrc" "$HOME/.vimrc"
+
+# install plugins
+vim +PlugInstall +qa!
+vim +CocInstall coc-json coc-python coc-snippets coc-vimlsp +qa!
