d4cb2d94fecf1a19563396775c3d675205cbc4e3
Angelo Arcillas <angeloarcillas64@gmail.com>
2022-06-13 19:52:11 +0800

    remove: unnecessary aliases

--- linux/.config/bashrc/aliases/package-manager.sh
+++ linux/.config/bashrc/aliases/package-manager.sh
@@ -1,42 +1,49 @@
 #!/bin/bash
 
-# npm
-alias n='npm'
-alias ni='npm install'
-alias nid='npm install -D'
-alias nig='npm install -g'
-alias nu='npm update'
-alias nau='npm audit'
-alias nauf='npm audit fix'
+# # npm
+# alias n='npm'
+# alias ni='npm install'
+# alias nid='npm install -D'
+# alias nig='npm install -g'
+# alias nu='npm update'
+# alias nau='npm audit'
+# alias nauf='npm audit fix'
 
 # npm script
-alias nrs='npm run serve'
-alias nrw='npm run watch'
-alias nrt='npm run test'
-alias nrd='npm run dev'
-alias nrb='npm run build'
-alias nrp='npm run production'
+# alias nrs='npm run serve'
+# alias nrw='npm run watch'
+# alias nrt='npm run test'
+# alias nrd='npm run dev'
+# alias nrb='npm run build'
+# alias nrp='npm run production'
+alias npmr='npm run'
 
 # yarn
-alias y='yarn'
-alias yi='yarn install'
-alias ya='yarn add'
-alias yad='yarn add -D'
-alias yau='yarn audit'
-alias yauf='yarn audit fix'
+# alias y='yarn'
+# alias yi='yarn install'
+# alias ya='yarn add'
+# alias yad='yarn add -D'
+# alias yau='yarn audit'
+# alias yauf='yarn audit fix'
 
 # yarn script
-alias yrs='yarn run serve'
-alias yrw='yarn run watch'
-alias yrt='yarn run test'
-alias yrd='yarn run dev'
-alias yrb='yarn run build'
-alias yrp='yarn run production'
+# alias yrs='yarn run serve'
+# alias yrw='yarn run watch'
+# alias yrt='yarn run test'
+# alias yrd='yarn run dev'
+# alias yrb='yarn run build'
+# alias yrp='yarn run production'
+alias yarnr='yarn run'
 
 # Funtions
 
 # npm install & watch
 niw() {
     npm install
-    npm run wath
+    npm run watch
+}
+
+yiw() {
+    yarn
+    yarn run watch
 }
